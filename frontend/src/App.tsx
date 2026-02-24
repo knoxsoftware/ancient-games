3bcec9e5f22ec3384132f4f32558ab2e98cafa48
Matt <matt1988@gmail.com>
2026-02-23 23:08:28 -0700

    feat: wrap app in ThemeProvider, add fixed ThemeToggle on non-game pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import SessionLobby from './components/lobby/SessionLobby';
 import GameRoom from './components/GameRoom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+
+function AppShell() {
+  const location = useLocation();
+  const isGameRoom = location.pathname.startsWith('/game/');
+
+  return (
+    <div className="min-h-screen" style={{ background: 'var(--bg-page)' }}>
+      {/* Fixed toggle on all pages except GameRoom (GameRoom has it inline) */}
+      {!isGameRoom && <ThemeToggle fixed />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/session/:sessionCode" element={<SessionLobby />} />
+        <Route path="/game/:sessionCode" element={<GameRoom />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/session/:sessionCode" element={<SessionLobby />} />
-          <Route path="/game/:sessionCode" element={<GameRoom />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <AppShell />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
