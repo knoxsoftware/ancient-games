0f6bbaa4617e554f6d22a9a2755e44e0024c748e
Matt <matt@knox.software>
2026-03-05 05:51:31 -0700

    feat(backend): register Bombermage in GameRegistry

--- backend/src/games/GameRegistry.ts
+++ backend/src/games/GameRegistry.ts
@@ -9,6 +9,7 @@ import { FoxAndGeeseGame } from './fox-and-geese/FoxAndGeeseGame';
 import { MancalaGame } from './mancala/MancalaGame';
 import { GoGame } from './go/GoGame';
 import { UrRoguelikeGame } from './ur-roguelike/UrRoguelikeGame';
+import { BombermageGame } from './bombermage/BombermageGame';
 import { GameType } from '@ancient-games/shared';
 
 export class GameRegistry {
@@ -23,6 +24,7 @@ export class GameRegistry {
     ['mancala', new MancalaGame() as GameEngine],
     ['go', new GoGame() as GameEngine],
     ['ur-roguelike', new UrRoguelikeGame() as GameEngine],
+    ['bombermage', new BombermageGame() as GameEngine],
   ]);
 
   static getGame(gameType: GameType): GameEngine {
